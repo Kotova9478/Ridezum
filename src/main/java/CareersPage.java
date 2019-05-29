39fbb59111e9eeee55f29931d535ca2f60f6c495
Kotova9478 <Kotova9478@gmail.com>
2019-05-28 19:40:53 -0700

    test jenkins3

--- src/main/java/CareersPage.java
+++ src/main/java/CareersPage.java
@@ -1,21 +1,21 @@
-import org.openqa.selenium.ElementClickInterceptedException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-
-public class CareersPage extends BasePage{
-    public CareersPage (WebDriver driver){
-        super(driver);
-
-
-    }
-    @FindBy(css = ".m-btn.m-banner__btn")
-    private WebElement viewJobs;
-
-    public JobsPage clickViewJobs(){
-       viewJobs.click();
-       return new JobsPage();
-
-    }
-
-}
+//import org.openqa.selenium.ElementClickInterceptedException;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.support.FindBy;
+//
+//public class CareersPage extends BasePage{
+//    public CareersPage (WebDriver driver){
+//        super(driver);
+//
+//
+//    }
+//    @FindBy(css = ".m-btn.m-banner__btn")
+//    private WebElement viewJobs;
+//
+//    public JobsPage clickViewJobs(){
+//       viewJobs.click();
+//       return new JobsPage();
+//
+//    }
+//
+//}
