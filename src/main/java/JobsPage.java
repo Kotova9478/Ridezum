17237dff2c601299b0173c2066c7c2f70c186483
Kotova9478 <Kotova9478@gmail.com>
2019-05-28 19:38:18 -0700

    test jenkins2

--- src/main/java/JobsPage.java
+++ src/main/java/JobsPage.java
@@ -1,24 +1,24 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-
-import java.util.List;
-
-public class JobsPage extends BasePage {
-    public JobsPage (WebDriver driver){
-        super(driver);
-    }
-    @FindBy(css = ".postings-btn.template-btn-submit.hex-color")
-    private List<WebElement> applayThisJob;
-    // topButton[0];
-    //        [1];
-
-    public String Applay(){
-        //wait.until(E)
-        applayThisJob.get(0).click();
-       String page = applayThisJob.get(0).getText();
-       return page;
-
-
-    }
-}
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.support.FindBy;
+//
+//import java.util.List;
+//
+//public class JobsPage extends BasePage {
+//    public JobsPage (WebDriver driver){
+//        super(driver);
+//    }
+//    @FindBy(css = ".postings-btn.template-btn-submit.hex-color")
+//    private List<WebElement> applayThisJob;
+//    // topButton[0];
+//    //        [1];
+//
+//    public String Applay(){
+//        //wait.until(E)
+//        applayThisJob.get(0).click();
+//       String page = applayThisJob.get(0).getText();
+//       return page;
+//
+//
+//    }
+//}
